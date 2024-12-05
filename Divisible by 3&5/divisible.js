20720418c3b7e9de0c01883f957876afb97b1b72
alizaiter777 <alizaiter777@gmail.com>
2024-12-05 23:12:59 +0200

    spelling

--- Divisible by 3&5/divisible.js
+++ Divisible by 3&5/divisible.js
@@ -1,7 +1,7 @@
 
 const input=prompt("Enter a number");
     for(let i=0;i<input;i++){
-        f(i%3==0 && i%5==0){
+        if(i%3==0 && i%5==0){
             console.log(i);
             }
         }
